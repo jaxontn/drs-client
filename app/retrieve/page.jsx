344f40bbf7938f85e604ba17c77cdc0436acd4c6
jaxontn <jaxontn.tech@gmail.com>
2024-01-18 21:45:20 +0800

    updated sort again

--- app/retrieve/page.jsx
+++ app/retrieve/page.jsx
@@ -55,12 +55,12 @@ const Home = () => {
                 if (data.success) {
 
                     //setPurchaseData(data.data);
-                    // Sort the array so that items with 'redeemed: true' come first
+                    // Sort the array so that items with 'redeemed: false' come first
                     const updatedPurchaseData = data.data.sort((a, b) => {
-                        // If 'a.redeemed' is true and 'b.redeemed' is false, put 'a' first.
-                        // If 'a.redeemed' is false and 'b.redeemed' is true, put 'b' first.
+                        // If 'a.redeemed' is false and 'b.redeemed' is true, put 'a' first.
+                        // If 'a.redeemed' is true and 'b.redeemed' is false, put 'b' first.
                         // For other cases, maintain the order.
-                        return a.redeemed === b.redeemed ? 0 : a.redeemed ? -1 : 1;
+                        return a.redeemed === b.redeemed ? 0 : a.redeemed ? 1 : -1;
                     });
 
                     setPurchaseData(updatedPurchaseData);
